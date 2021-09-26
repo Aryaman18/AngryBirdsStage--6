29ba644cc9bfbc02e9850940c5054abda41b5fb3
anu203m <78315583+anu203m@users.noreply.github.com>
2021-09-26 20:37:34 +0530

    Update Pig.js

--- Pig.js
+++ Pig.js
@@ -2,10 +2,11 @@ class Pig extends BaseClass {
   constructor(x, y){
     super(x,y,50,50);
     this.image = loadImage("sprites/enemy.png");
-    this.Visiblity = 255;
+    
   }
 
- 
+ display(){
+ }
 
 
 };
